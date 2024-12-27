2aee4250e0016a2f666d82f630fc69bb44acf308
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-12-27 10:29:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'; 
 import { createStore,applyMiddleware, compose } from 'redux' 
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'    
