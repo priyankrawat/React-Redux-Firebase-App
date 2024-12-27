e80923c92365bcb79b69d794e5676ede05e270db
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-12-27 10:30:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker'; 
 import { createStore,applyMiddleware, compose } from 'redux'  
 import rootReducer from './store/reducers/rootReducer'
-import { Provider } from 'react-redux'    
+import { Provider } from 'react-redux'     
 import thunk from 'redux-thunk' 
 import { reduxFirestore, getFirestore } from 'redux-firestore'
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase' 
