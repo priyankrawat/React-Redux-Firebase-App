3d4722d810ae04f1269fd44c337fd611a366e1cd
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2025-01-12 18:26:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn' 
 import SignUp from './components/auth/SignUp'
 import CreateProject from './components/projects/CreateProject'
-
+ 
 
 class App extends Component {
   render(){
